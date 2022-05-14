e7f1dcf893b6466ef15c7c6e4d4c6d5ac08bcdc7
akashwebs <aksah.shil227@gmail.com>
2022-05-14 19:47:13 +0600

    added info card

---
+++ src/Pages/Home/Info/Info.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import InfoCard from '../InfoCard/InfoCard';
+import clock from '../../../assets/icons/clock.svg'
+import marker from '../../../assets/icons/marker.svg'
+import phone from '../../../assets/icons/phone.svg'
+
+
+
+const Info = () => {
+
+    const infos = [
+        {
+            img: clock,
+            bgColor: 'bg-gradient-to-r from-secondary to-primary',
+            title: 'Opening Hours',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, soluta.'
+        },
+        {
+            img: marker,
+            bgColor: 'bg-accent',
+            title: 'Opening Hours',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, soluta.'
+        },
+        {
+            img: phone,
+            bgColor: 'bg-gradient-to-r from-secondary to-primary',
+            title: 'Opening Hours',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, soluta.'
+        }
+    ]
+
+
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-5 px-12'>
+            {infos.map(info=><InfoCard info={info}></InfoCard>)}
+        </div>
+    );
+};
+
+export default Info;
